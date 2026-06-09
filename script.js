0ae6e9a77a8dc2122eb6b293e259843237e4bb37
JOTHILAKSHMI11 <jothilakshu11@gmail.com>
2026-06-09 10:44:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-let btn = document.getElementById("button");
+function downloadPDF() {
+    const downloadButton = document.getElementById("button");
+    const element = document.getElementById("content");
 
+    if (downloadButton) {
+        downloadButton.style.display = 'none';
+    }
 
-
-btn.addEventListener("click", () => {
-    const { jsPDF } = window.jspdf;
-
-    const doc = new jsPDF();
-
-    doc.autoTable({
-        html: "table"
-    });
-
-    doc.save("table.pdf");
-});
+    html2pdf()
+        .set({
+            margin: 9,
+            filename: 'portfolio.pdf',
+            image: { type: 'jpeg', quality: 1 },
+            html2canvas: { scale: 2, useCORS: true },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        })
+        .from(element)
+        .save()
+        .then(() => {
+            if (downloadButton) {
+                downloadButton.style.display = 'block'; 
+            }
+        });
+}
